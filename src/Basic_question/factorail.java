2e082d658e2f5fd4e6bff8b609e1ad00f8417f7b
saadahmedbd <saadahmedbd0@gmail.com>
2024-07-03 17:14:36 +0600

    factorail

---
+++ src/Basic_question/factorail.java
@@ -0,0 +1,17 @@
+package Basic_question;
+
+public class factorail {
+    public static void fact(int n){
+        int n1=1;
+        if(n<0){
+            System.out.println("invalid");
+        }
+        for (int i=1;i<=n;i++){
+            n1=n1+i;
+        }
+        System.out.println(n1);
+    }
+    public static void main(String[] args) {
+        fact(-1);
+    }
+}
