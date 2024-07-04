57163f01470db3205c61dd67e78cc81503e5529e
saadahmedbd <saadahmedbd0@gmail.com>
2024-07-04 10:49:36 +0600

    fibonacci

--- src/Basic_question/fibonacci_series.java
+++ src/Basic_question/fibonacci_series.java
@@ -7,7 +7,18 @@ public class fibonacci_series {
       }
       return fibonacci(n-1) +fibonacci(n-2);
     }
+    public static void fibo(int n1){
+        int n2=0;
+        int n3=1;
+        for(int i=0;i<n1;i++){
+            System.out.println(n3+" ");
+            int result =n2+n3;
+            n2=n3;
+            n3=result;
+        }
+    }
     public static void main(String[] args) {
         System.out.println(fibonacci(1));
+        fibo(9);
     }
 }
