f67e965968c3691ecaebc541a3086bee3f4f7b73
saadahmedbd <saadahmedbd0@gmail.com>
2024-07-04 10:38:37 +0600

    fibonacci

---
+++ src/Basic_question/fibonacci_series.java
@@ -0,0 +1,13 @@
+package Basic_question;
+
+public class fibonacci_series {
+    public static int fibonacci(int n){
+      if(n==0 || n==1){
+          return n;
+      }
+      return fibonacci(n-1) +fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+        System.out.println(fibonacci(1));
+    }
+}
